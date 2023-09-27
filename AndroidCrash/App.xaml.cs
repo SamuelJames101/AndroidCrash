9ffd216a6050ffecb4c4ccff1186158608c87b1a
samueljames <samuel.james@bt.com>
2023-09-27 16:28:40 +0100

    First commit

---
+++ AndroidCrash/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AndroidCrash;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AndroidCrash/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidCrash;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
